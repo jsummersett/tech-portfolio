2b91cf6fef14b6add7d8c5d3b9beed878754f2c0
jsummersett <54999070+jsummersett@users.noreply.github.com>
2020-10-07 12:11:34 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Josephine Summersett', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Student at Vassar College | Computer Science Teaching Assistant | Aspiring technologist',
     profilePhoto: 'img/headshot.jpg',
     url: 'https://jsummersett.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
